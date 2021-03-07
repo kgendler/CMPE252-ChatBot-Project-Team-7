e17bf4cdcc903fe4f3a0eb11f0583ae8884700e5
Karl Gendler <karl@Karls-MacBook-Air.local>
2021-03-06 16:44:29 -0800

    Prep for push to main branch

---
+++ FridgeLogic.py
@@ -0,0 +1,206 @@
+from chatterbot import filters
+from chatterbot.comparisons import LevenshteinDistance, JaccardSimilarity, SynsetDistance
+from chatterbot.conversation import Statement
+from chatterbot.logic import LogicAdapter, BestMatch
+from refrigerator_world_training import conversations
+
+
+class PreviousInputLogic(LogicAdapter):
+
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+
+        self.previous_input = Statement('')
+
+    def can_process(self, statement):
+        if self.previous_input.text != '':
+            return True
+        else:
+            self.previous_input = statement  # Do not run the first time
+            return False
+
+    def process(self, input_statement, additional_response_selection_parameters):
+        """Doing a really poor job of memory management here but I don't think it's critical for the current scope"""
+
+        if 'order' in input_statement.text.lower():
+            output = Statement ('')
+            output.confidence = 0
+            return output
+
+        # These are classes so need to instantiate them before using their compare method
+        LevDist = LevenshteinDistance()
+        # SynDist = SynsetDistance()
+
+        # These will store max values found so far as we iterate through our trained conversations
+        LevDist_max = 0
+        # SynDist_max = 0
+
+        store_i = 0
+        store_j = 0
+        # Iterate through the conversations and entries in each, tracking the best match
+        for i, conversation in enumerate(conversations):
+            for j, entry in enumerate(conversation):
+                if (j % 2) == 1:  # only analyze human inputs
+                    continue
+
+                if (j + 3) >= len(conversation):  # make sure enough entries remain in conversation for the logic below
+                    continue
+
+                statement = Statement(entry)  # must be Statement object for comparisons, not string
+
+                LD = LevDist.compare(self.previous_input, statement)
+                if LD > LevDist_max:
+                    store_i = i
+                    store_j = j
+                    LevDist_max = LD
+                    LD_statement = Statement(conversation[j+3])
+
+                # SD = SynDist.compare(self.previous_input, statement)
+                # if SD > SynDist_max:
+                #     if SynDist.compare(Statement(conversation[i+2]), input_statement) > 0.9:
+                #         SD += (1 - SD)/2  # give boost over competition if current statment is a good match in the right position in the same conversation
+                #     SynDist_max = SD
+                #     SD_statement = Statement(conversation[i+3])
+
+
+        if LevDist.compare(Statement(conversations[store_i][store_j+2]), input_statement) > 0.9:
+            LD = 10  # if current statment is a good match in the right position in the same conversation, it's probably right.
+
+        # Having calculated the maximum confidences for each method, determine the winner and output that Statement object
+        # Note that output here doesn't mean it'll be the final chosen output of the bot. Bot will compare confidence from
+        # this and any other logic adapters and choose the output with the highest confidence.
+        # if LevDist_max > SynDist_max:
+        output = LD_statement
+        output.confidence = LevDist_max
+
+        # elif SynDist_max > LevDist_max:
+        #     output = SD_statement
+        #     output.confidence = SynDist_max
+
+        # else:
+        #     output = Statement('Not quite sure how to respond to that...')
+        #     output.confidence = 0.1
+        
+        # print(f'LevDist: {LevDist_max}, {LD_statement}')
+        # print(f'SynDist: {SynDist_max}, {SD_statement}')
+        print('PreviousLogicAdapter returning {} with confidence {}'.format(output.text, output.confidence))
+        self.previous_input = input_statement
+        return output
+
+
+class OrderLogic(LogicAdapter):
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+
+    def can_process(self, statement):
+        """Figure out if the statement from the user contains the word "order".
+        
+        There's gotta be a better way to decide whether or not to run this."""
+        if 'order' in statement.text.lower():
+            return True
+        else:
+            return False
+
+    def process(self, input_statement, additional_response_selection_parameters):
+        """Determine how to respond."""
+        search_results = self.search_algorithm.search(input_statement)
+
+        # Search for the closest match to the input statement
+        confidence = 0
+        closest_match = search_results[0]
+        for result in search_results:
+            if result.confidence > confidence:
+                confidence = result.confidence
+                closest_match = result
+
+        # Get the best first response
+        response_list = list(self.chatbot.storage.filter(search_in_response_to=closest_match.search_text))
+        response = response_list[0]
+
+        if response.text == 'createorder':
+            item = input('Sounds like you want to place an order. What would you like to order?\n')
+            quantity = input('What quanitity would you like to order?\n')
+            delivery = input('What would you like the order delivered? You can choose morning, afternoon, or evening.\n')
+            self.chatbot.createOrder(item.lower(), quantity, delivery)
+            if 'milk' in item.lower():
+                item = 'gallon(s) of milk'
+            elif 'bacon' in item.lower():
+                item = 'pound(s) of bacon'
+            else:
+                item = 'egg(s)'
+
+            response = Statement('Great! Your order of {} {} will be delivered in the {}.'.format(quantity, item, delivery))
+
+        elif response.text == 'modifyorder':
+            order = self.chatbot.order
+            delivery = input('Okay, I see your order for {} coming in the {}. When would you like the order to be delivered? You can choose morning, afternoon, or evening.\n'.format(order.item, order.delivery_time))
+            order.delivery_time = delivery
+            if 'milk' in order.item:
+                item = 'gallon(s) of milk'
+            elif 'bacon' in order.item:
+                item = 'pound(s) of bacon'
+            else:
+                item = 'egg(s)'
+            response = Statement('All set! Your order of {} {} will now be delivered in the {}.'.format(order.quantity, item, delivery))
+
+        elif response.text == 'orderstatus':
+            order = self.chatbot.order
+            if not order:
+                response = Statement('You have no pending order.')
+            else:
+                if 'milk' in order.item:
+                    item = 'gallon(s) of milk'
+                elif 'bacon' in order.item:
+                    item = 'pound(s) of bacon'
+                else:
+                    item = 'egg(s)'
+                response = Statement('Your order of {} {} is scheduled to be delivered in the {}.'.format(order.quantity, item, order.delivery_time))
+            
+        elif response.text == 'deleteorder':
+            order = self.chatbot.order
+            if not order:
+                response = Statement('You have no pending order.')
+            else:
+                item = order.item
+                self.chatbot.order = None
+                response = Statement("I've cancelled your order for {}.".format(item))
+
+        response.confidence = 1.2
+        return response
+
+
+class TestLogic(LogicAdapter):
+
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+
+    def can_process(self, statement):
+        print(statement.conversation)
+        recent_repeated_responses = filters.get_recent_repeated_responses(
+            self.chatbot,
+            statement.conversation
+        )
+        print(recent_repeated_responses)
+        statements_in_db = list(self.chatbot.storage.filter())
+        for statement_in_db in statements_in_db:
+            print(statement_in_db.conversation)
+        return False
+
+
+if __name__ == '__main__':
+    from chatterbot import ChatBot
+    from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+    testbot = ChatBot('Test', logic_adapters=['FridgeLogic.TestLogic', 'chatterbot.logic.BestMatch'])
+    #trainer = ChatterBotCorpusTrainer(testbot)
+    #trainer.train('chatterbot.corpus.english.greetings')
+    trainer = ListTrainer(testbot)
+    for conversation in conversations:
+        trainer.train(conversation)
+    print('Welcome to FridgeBot! How may I be of assistance?')
+    user_input = input()
+    while user_input:  # if user enters blank response, chatbot exits.
+        response = testbot.get_response(user_input)
+        print(response)
+        user_input = input()
+    else:
+        print('Now exiting FridgeBot. Enjoy your breakfast!')
--- refrigerator_world_training.py
+++ refrigerator_world_training.py
@@ -1,144 +1,190 @@
+from chatterbot import ChatBot
+from random import randrange
+
 conversations = [[
     'Where is the milk?',
-    'Regular or Non-Fat?',
-    'Regular.',
-    'It is in the refrigerator, on the left side.'
-],
-[
-    'Where is the milk?',
-    'Regular or Non-Fat?',
-    'Non-Fat.',
-    'I’m not sure.  Maybe there is some in the freezer.'
-],
-[
-    'Where is the Regular milk?',
-    'There should be some in the refrigerator.',
+    'There should be some in the refrigerator on the left side.',
     'I am not finding any.',
-    'Did you look on the right side?'
+    'fridgecontents-milk'
 ],
 [
-    'Where is the Non-Fat milk?',
-    'I think we are out.  Should I order some?',
-    'Yes, please.',
-    'Ok.  I will ask them to deliver it today.'
+    'What do we have in the refrigerator?',
+    'fridgecontents'
 ],
-[
-    'Do we have any Non-Fat milk?',
-    'We have powdered Non-Fat milk in the garage.',
-    'Is there any in the refrigerator?',
-    'No. Just in the garage.'  # This response was not given. "I am not finding any." was the bot's reply.
-],
-[
-    'What do we have in the refrigerator to eat?',
-    'Milk, eggs and bacon.',
-    'What kind of milk',
-    'Regular.'
+[    
+    'What is in the fridge?',
+    'fridgecontents'
 ],
 [
     'Are we out of bacon?',
-    'Did you look in the refrigerator?',
-    'Yes, but I didn’t see any.',
-    'Did you look behind the milk?'
-],
-[
-    'Do we have any hard boiled eggs?',
-    'Yes, in the refrigerator',
-    'I don’t see them.',
-    'Did you look behind the milk?'  # bot replied with "What kind of movies do you like?"
-],
-[
-    'Do we have any uncooked eggs?',
-    'What kind?',
-    'Brown eggs.',
-    'No.  We don’t have any brown eggs.',
-    'Why not?',
-    'You didn’t buy any.'  # bot replied with "Because I have never been inside the refrigerator when the door was closed."
-],
-[
-    'Do we have any uncooked eggs?',
-    'What kind?',
-    'Any kind.',
-    'Look behind the milk.',
-    'Oh, there it is.  Thank you.',
-    'What would you do without me?'
-],
-[
-    'Who ate all of the eggs?',
-    'We have eggs.',
-    'Where?',
-    'Did you look behind the milk?',
-    'Oh, there it is.  Thank you.',
-    'What would you do without me?'
+    'fridgecontents-bacon'
 ],
 [
     'Do we have any bacon left',
-    'Did you look behind the milk?',
-    'No.  I don’t need to look behind the milk.',
-    'Please, look behind the milk.',
-    'Oh, there it is.  Thank you.',
-    'Try looking behind the milk before you ask me next time.'  # bot replied with "What would you do without me?"
+    'fridgecontents-bacon'
 ],
-[  # Karl: I think we should omit this conversation... Do we have milk, did you look being the milk... no milk.. kind of confusing. (But funny!)
+[
+    'Do we have any eggs?',
+    'fridgecontents-eggs'
+],
+[
+    'Who ate all of the eggs?',
+    "Well it certainly wasn't me!"
+],
+[ 
     'Do we have any milk?',
-    'Did you look behind the milk?',  # Bot replied with "Regular or Non-Fat?"
-    'No.',
-    'Please, look behind the milk.',
-    'Ok. I am looking behind the milk but all I see are eggs and bacon.',
-    'I guess we are out of milk, then.'
+    'fridgecontents-milk'
 ],
 [
     'Where is the chocolate milk? ',
-    'We don’t have chocolate milk, only regular or non-fat milk.',
+    'We don’t have chocolate milk, only regular milk.',
     'Where is the regular milk?',
-    'In the refrigerator, in front of the bacon.'  # bot replied with "There should be some in the refrigerator."
+    'In the refrigerator, on the left side.'  # bot replied with "There should be some in the refrigerator."
 ],
 [
     'Do we have any frozen pizza?',
-    'No.  You ate it all.',
+    'fridgecontents'
+],
+[
     'Are you sure?',
-    'Yes, I am sure.',  # bot replied with "Just because I'm software does not mean that I cannot experience existence."
-    'Are you really sure?',
-    'Yes, I am really sure.  Go do your homework.'
+    'Yes, I am sure.  Go do your homework ya lazy bum.'
 ],
 [
     'Where should I put the milk?',
-    'In the refrigerator.',
-    'Where in the refrigerator?',
-    'Put the milk in front of the eggs, or in front of the bacon.',  # bot replied with "I am not finding any."
-    'Ok, is there anything else in the refrigerator?',
-    'No, just eggs, milk and bacon.'
+    'In the refrigerator, on the left side.',
 ],
 [
     'Do we have any deviled eggs?',
-    'No, we only have raw or hard-boiled eggs.',
+    'No, we only have raw eggs.',
     'Why don’t we have any deviled eggs?',
     'I don’t know.  You can make some yourself if you want them.',
     'I am too lazy to cook right now.',
-    'Then no devilled-eggs for you.',
+    'Then no devilled-eggs for you.'
 ],
 [
     'I am hungry',
-    'Why don’t you get some food out of the refrigerator?',
-    'I only see milk in the refrigerator.',
-    'Are you sure?',
-    'Yes.  I am sure.',
-    'Did you look behind the milk?'
+    'Why don’t you get some food out of the refrigerator?'
 ],
 [
     'Does the refrigerator light stay on after I close the door?',
-    'I don’t know.',
+    'I don’t know, I have never been inside the refrigerator when the door was closed.',
     'Why not?',
-    'Because I have never been inside the refrigerator when the door was closed.',
-    'Why not?',
-    'Because it is dangerous.'  # Bot replies with "Because I have never been inside the refigerator when the door was closed."
+    'Because it is dangerous. Let us never speak of this again.'  # Bot replies with "Because I have never been inside the refigerator when the door was closed."
 ],
 [
-    'How many eggs do we have in the refrigerator?',
-    'We should have 12 eggs.',
-    'I don’t see any',
-    'Look behind the milk.',  # Bot replied with "What kind of movies do you like?"
-    'I only see bacon behind the milk.',
-    'Look behind the bacon.'
-]
+    'What is the status of my order',
+    'orderstatus'
+],
+[
+    'Reschedule my order',
+    'modifyorder',
+],
+[
+    'Schedule an order',
+    'createorder'
+],
+[
+    "I'd like to place an order",
+    'createorder'
+],
+[
+    'Cancel the order',
+    'deleteorder'
+],
+[
+    'Thank you',
+    'You are welcome!'
+],
+[
+    'Where is my order?',
+    'orderstatus'
+],
+[
+    'My order has arrived!',
+    'updatefridge'
+],
 ]
 
+class Order(object):
+    def __init__(self, item, units, quantity, delivery_time):
+        self.item = item
+        self.units = units
+        self.quantity = quantity
+        self.delivery_time = delivery_time
+
+class FridgeBot(ChatBot):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        self.order = None
+        self.milk = randrange(1,2)
+        self.eggs = randrange(1,24)
+        self.bacon = randrange(1,5)
+
+        # self.inputs = []
+        # self.best_matches = []
+        # self.outputs = []
+
+    def createOrder(self, item, units, quantity, delivery_time):
+        """Add an order for an item.
+
+        In the future, can append new order to a list, add other functionality to handle multiple orders."""
+        self.order = Order(item, units, quantity, delivery_time)
+
+    # Here lies unimplemented functionality. RIP.
+
+#                  ______
+#            _____/      \\_____
+#           |  _     ___   _   ||
+#           | | \     |   | \  ||
+#           | |  |    |   |  | ||
+#           | |_/     |   |_/  ||
+#           | | \     |   |    ||
+#           | |  \    |   |    ||
+#           | |   \. _|_. | .  ||
+#           |                  ||
+#           |    functions     ||
+#           |                  ||
+#   *       | *   **    * **   |**      **
+#    \))..),,/.,(//,,..,,\||(,,.,\\,.((//
+
+    # def consume(self, item, amount):
+    #     """Remove an amount of milk, eggs, or bacon from the fridge."""
+    #     if item == 'milk':
+    #         self.milk -= amount
+    #         if self.milk < 0:
+    #             self.milk == 0
+    #     elif item == 'eggs':
+    #         self.eggs -= amount
+    #         if self.eggs < 0:
+    #             self.eggs == 0
+    #     elif item == 'bacon':
+    #         self.bacon -= amount
+    #         if self.bacon < 0:
+    #             self.bacon == 0
+    
+    # def store(self, item, amount):
+    #     """Add an amount of milk, eggs, or bacon to the fridge."""
+    #     if item == 'milk':
+    #         self.milk += amount
+    #     elif item == 'eggs':
+    #         self.eggs += amount
+    #     elif item == 'bacon':
+    #         self.bacon += amount
+
+    # def logInput(self, user_input):
+    #     """Add to the list of the last up to 10 user inputs"""
+    #     self.inputs.append(user_input)
+    #     if len(self.inputs) > 10:
+    #         self.inputs = self.inputs[:10]
+    
+    # def logBestMatch(self, best_match):
+    #     """Add to the list of the last up to 10 best matches to the user inputs"""
+    #     self.best_matches.append(best_match)
+    #     if len(self.best_matches) > 10:
+    #         self.best_matches = self.best_matches[:10]
+
+    # def logOutput(self, output):
+    #     """Add to the list of the last up to 10 bot outputs"""
+    #     self.outputs.append(output)
+    #     if len(self.outputs) > 10:
+    #         self.outputs = self.outputs[:10]
